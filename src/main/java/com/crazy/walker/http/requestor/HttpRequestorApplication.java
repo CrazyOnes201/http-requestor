f777fc47101d4aca4a384916ab89f1037a861f78
CrazyOnes201 <kanzhaoyang123@qq.com>
2020-11-01 00:37:17 +0800

    #comment 引入hutool-core 作为雪花算法生成包 完成雪花算法工具类

--- src/main/java/com/crazy/walker/http/requestor/HttpRequestorApplication.java
+++ src/main/java/com/crazy/walker/http/requestor/HttpRequestorApplication.java
@@ -2,8 +2,9 @@ package com.crazy.walker.http.requestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HttpRequestorApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/crazy/walker/http/requestor/util/SnowflakeUtil.java
@@ -0,0 +1,26 @@
+package com.crazy.walker.http.requestor.util;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
+
+/**
+ * *****************************************
+ * **       @author : CrazyWalker         **
+ * *****************************************
+ * *****************************************
+ * **@date: 周日,11/01 2020 12:22 上午GMT+8**
+ * *****************************************
+ * *****************************************
+ * **         用途: 雪花算法工具类          **
+ * *****************************************
+ */
+public class SnowflakeUtil {
+
+    private static final Long DEFAULT_WORKER_ID = 1L;
+    private static final Long DEFAULT_DATA_CENTER_ID = 1L;
+    private static final Snowflake DEFAULT_SNOWFLAKE = IdUtil.getSnowflake(DEFAULT_WORKER_ID, DEFAULT_DATA_CENTER_ID);
+
+    public static long create() {
+        return DEFAULT_SNOWFLAKE.nextId();
+    }
+}
