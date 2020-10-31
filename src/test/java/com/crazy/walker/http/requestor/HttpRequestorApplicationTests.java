afc30b8244fff551ac9dcefbc321886a83c2518a
CrazyOnes201 <kanzhaoyang123@qq.com>
2020-10-31 23:17:10 +0800

    #comment Spring-boot工程初始化

---
+++ src/test/java/com/crazy/walker/http/requestor/HttpRequestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazy.walker.http.requestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
